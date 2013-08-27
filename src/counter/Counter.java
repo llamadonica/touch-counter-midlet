dcc3089322ad3395856eeed8f1c38ca582cdb92a
Adam Stark <llamadonica@gmail.com>
2013-08-27 09:57:34 -0700

    Simplified to use "StringItem"
    Signed-off-by: Adam Stark <llamadonica@gmail.com>

--- src/counter/Counter.java
+++ src/counter/Counter.java
@@ -11,12 +11,12 @@ import javax.microedition.lcdui.*;
  * @author Adam
  */
 public class Counter 
-extends CustomItem
+extends StringItem
 implements ItemCommandListener {
     public int current_value ;
     private Command command_inc;
     public Counter(int value) {
-        super("" + value);
+        super("Count", "" + value,Item.BUTTON);
         current_value = value;
     }
     public void initialize () {
@@ -25,31 +25,7 @@ implements ItemCommandListener {
         setDefaultCommand(command_inc);
     }
     public void setValue (int value) {
-        setLabel("" + value);
-    }
-    protected void paint (Graphics g, int x, int y) {
-        int x_g    = g.getClipX();
-        int y_g    = g.getClipY();
-        int width  = g.getClipWidth();
-        int height = g.getClipHeight();
-        g.setColor(Display.COLOR_BACKGROUND);
-        g.fillRect(x_g, y_g, width, height);
-    }
-    protected int getPrefContentWidth(int height) {
-        Font working_font = Font.getDefaultFont();
-        return working_font.stringWidth("" + current_value);
-    }
-    protected int getMinContentWidth() {
-        return 0;
-    }
-    protected int getPrefContentHeight(int width) {
-        return 0;
-    }
-    protected int getMinContentHeight() {
-        return 0;
-    }
-    public void pointerPressed(int x, int y) {
-        setValue(++current_value);
+        setText("" + value);
     }
     public void commandAction(Command c, Item item) {
         if (c == command_inc) {
